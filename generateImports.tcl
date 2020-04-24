9e1ebe625a72c69bac8c4d53b5a176cadf1014c5
mm <michmajchrzak@gmail.com>
2020-04-24 15:40:25 +0200

    updte generateImports

--- generateImports.tcl
+++ generateImports.tcl
@@ -2,7 +2,7 @@ puts "I am generating imports for $argv0"
 puts "..."
 if {$argc == 1} {
     set arg1 $argv
-    regsub -all "/" $arg1  "." arg
-    regsub -all ".scm" $arg1 ".so" arg
+    regsub -all "/" $arg1  "." arg1
+    regsub -all .scm $arg1 .so arg1
     exec csc -J -s -o $arg1 $argv
 }
